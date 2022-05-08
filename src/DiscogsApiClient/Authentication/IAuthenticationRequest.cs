82c34904b8e263feed7ee9222a578cfdaccbf33a
Alexander Jurk <alexander.jurk@outlook.com>
2022-05-08 10:59:26 +0200

    Added comments

--- src/DiscogsApiClient/Authentication/IAuthenticationRequest.cs
+++ src/DiscogsApiClient/Authentication/IAuthenticationRequest.cs
@@ -1,3 +1,7 @@
 ﻿namespace DiscogsApiClient.Authentication;
 
+/// <summary>
+/// Parameters needed by the <see cref="IAuthenticationProvider"/> to authenticate against the Discogs Api.
+/// Implementations of the <see cref="IAuthenticationProvider"/> require the corresponding implementation of the <see cref="IAuthenticationRequest"/>.
+/// </summary>
 public interface IAuthenticationRequest { }
--- src/DiscogsApiClient/Authentication/IAuthenticationResponse.cs
+++ src/DiscogsApiClient/Authentication/IAuthenticationResponse.cs
@@ -1,8 +1,18 @@
 ﻿namespace DiscogsApiClient.Authentication;
 
+/// <summary>
+/// The response of the <see cref="IAuthenticationProvider"/>'s AuthenticateAsync method.
+/// Specific implementations my return more response properties depending on the authentication type.
+/// </summary>
 public interface IAuthenticationResponse
 {
+    /// <summary>
+    /// Indicates if the authentication was successful.
+    /// </summary>
     bool Success { get; }
 
+    /// <summary>
+    /// The error message if the authentication failed.
+    /// </summary>
     string? Error { get; }
 }
