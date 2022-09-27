87a96f543a556e1054b92ee56cbc2710f5013a82
Alexander Jurk <alexander.jurk@outlook.com>
2022-09-27 23:18:08 +0200

    Added Plain OAuth demo

---
+++ demo/DiscogsApiClientDemo.PlainOAuth/LoginWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Web.WebView2.Core;
+
+namespace DiscogsApiClientDemo.PlainOAuth;
+
+public partial class LoginWindow : Window
+{
+    private readonly string _loginUrl;
+    private readonly string _callbackUrl;
+
+    public string Result { get; set; }
+
+    public LoginWindow(string loginUrl, string callbackUrl)
+    {
+        _loginUrl = loginUrl;
+        _callbackUrl = callbackUrl;
+        Result = "";
+
+        InitializeComponent();
+    }
+
+    private async void Browser_Loaded(object sender, RoutedEventArgs e)
+    {
+        await Browser.EnsureCoreWebView2Async();
+        Browser.Source = new Uri(_loginUrl);
+    }
+
+    private void Browser_NavigationStarting(object sender, CoreWebView2NavigationStartingEventArgs e)
+    {
+        if (e.Uri.StartsWith(_callbackUrl))
+        {
+            int index = e.Uri.IndexOf("?");
+
+            Result = index > -1 ? e.Uri[(index + 1)..] : "";
+
+            Close();
+        }
+    }
+}
