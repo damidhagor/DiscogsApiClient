ef74e0838174bd09fbf0b72759745a4c6cc8e0db
Alexander Jurk <alexander.jurk@outlook.com>
2023-03-07 22:49:17 +0100

    Added unit tests for GetUser and GetIdentity

--- src/DiscogsApiClient.Tests/User/IdentityTestFixture.cs
+++ src/DiscogsApiClient.Tests/User/IdentityTestFixture.cs
@@ -17,4 +17,15 @@ public sealed class IdentityTestFixture : ApiBaseTestFixture
         Assert.AreEqual(12579295, identity.Id);
         Assert.AreEqual("https://api.discogs.com/users/DamIDhagor", identity.ResourceUrl);
     }
+
+    [Test]
+    public void GetIdentity_Unauthenticated()
+    {
+        var unauthenticatedClients = CreateUnauthenticatedDiscogsApiClient();
+
+        Assert.ThrowsAsync<UnauthenticatedDiscogsException>(() => unauthenticatedClients.discogsApiClient.GetIdentity(default));
+
+        unauthenticatedClients.authHttpClient.Dispose();
+        unauthenticatedClients.clientHttpClient.Dispose();
+    }
 }
--- src/DiscogsApiClient.Tests/User/UserTestFixture.cs
+++ src/DiscogsApiClient.Tests/User/UserTestFixture.cs
@@ -24,6 +24,27 @@ public sealed class UserTestFixture : ApiBaseTestFixture
     }
 
     [Test]
+    public async Task GetUser_Unauthenticated()
+    {
+        var username = "damidhagor";
+        var unauthenticatedClients = CreateUnauthenticatedDiscogsApiClient();
+
+        var user = await unauthenticatedClients.discogsApiClient.GetUser(username, default);
+
+        Assert.IsNotNull(user);
+        Assert.AreEqual(12579295, user.Id);
+        Assert.AreEqual("DamIDhagor", user.Username);
+        Assert.AreEqual(null, user.Email);
+        Assert.AreEqual("https://api.discogs.com/users/DamIDhagor", user.ResourceUrl);
+        Assert.IsTrue(user.IsActivated);
+        Assert.IsFalse(string.IsNullOrWhiteSpace(user.AvatarUrl));
+        Assert.IsFalse(string.IsNullOrWhiteSpace(user.CollectionFoldersUrl));
+
+        unauthenticatedClients.authHttpClient.Dispose();
+        unauthenticatedClients.clientHttpClient.Dispose();
+    }
+
+    [Test]
     public void GetUser_EmptyUsername()
     {
         var username = "";
