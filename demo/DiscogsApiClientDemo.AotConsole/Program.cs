28521e9063cd8510ae17fc335761a9b63ea1d3d7
Alexander Jurk <alexander.jurk@outlook.com>
2023-08-24 00:32:02 +0200

    Added Aot publishable console demo

---
+++ demo/DiscogsApiClientDemo.AotConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using DiscogsApiClient;
+using DiscogsApiClient.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+
+var personalAccessToken = "";
+
+var services = new ServiceCollection()
+    .AddDiscogsApiClient(options =>
+    {
+        options.UserAgent = "AwesomeAppDemo/1.0.0";
+        options.UseRateLimiting = true;
+    })
+    .BuildServiceProvider();
+
+var authService = services.GetRequiredService<IDiscogsAuthenticationService>();
+authService.AuthenticateWithPersonalAccessToken(personalAccessToken);
+
+var discogsApiClient = services.GetRequiredService<IDiscogsApiClient>();
+var identity = await discogsApiClient.GetIdentity(default);
+var user = await discogsApiClient.GetUser(identity.Username, default);
+
+Console.WriteLine($"User: {user.Username}");
+Console.WriteLine($"E-Mail: {user.Email}");
